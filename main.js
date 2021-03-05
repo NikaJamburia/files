eda3deaa4978b4f53cb84baf6f9f44188f7a9b40
Nika Jamburia <nikajamburia161@gmail.com>
2021-03-06 01:18:13 +0400

    aa

--- main.js
+++ main.js
@@ -3,23 +3,33 @@ function initMap() {
 
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 2, 
-        center: {lat: -19.257753, lng: 146.823688},
+        zoom: 10, 
+        center: {lat: 41.68234735080762, lng: 44.8471791124803},
     });
 
-    var kmlLayer = new google.maps.KmlLayer('https://nikajamburia.github.io/files/routes-adjara-sm.kml', {
-        suppressInfoWindows: true,
-        map: map
+    var allRoutesData = new google.maps.Data({map: map});
+
+    allRoutesData.loadGeoJson(
+        "https://nikajamburia.github.io/files/routes-adjara-sm.geojson"
+    );
+
+    allRoutesData.setStyle({
+        strokeColor: 'green',
+        strokeWeight: 3
+    });
+    
+    allRoutesData.addListener('click', function(event) {
+        allRoutesData.overrideStyle(event.feature, {strokeColor: 'red'});
+
+        console.log(event.feature.g.g[1]);
+
+
+        var routeMiddle = event.feature.g.g[Math.round((event.feature.g.g.length - 1) / 2)];
+        console.log(routeMiddle);
+        map.setCenter({ lat: routeMiddle.lat, lng: routeMiddle.lng });
+        map.setZoom(12);
+
+        console.log(event.feature);
     });
 
-    google.maps.event.addListener(kmlLayer,'status_changed',function() {
-        console.log(kmlLayer.getStatus());
-        console.log(kmlLayer);
-    });
-
-    // kmlLayer.addListener('click', function(event) {
-    // var content = event.featureData.infoWindowHtml;
-    // var testimonial = document.getElementById('capture');
-    // testimonial.innerHTML = content;
-    // });
   }
