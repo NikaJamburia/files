0cb768b1fee4ea8a4df0e7a735c411dbf75ca54b
Nika Jamburia <nikajamburia161@gmail.com>
2021-03-06 21:37:41 +0400

    aaaaaaa

--- main.js
+++ main.js
@@ -7,51 +7,54 @@ function initMap() {
         center: {lat: 41.68234735080762, lng: 44.8471791124803},
     });
 
-    var kmlLayer = new google.maps.KmlLayer("https://nikajamburia.github.io/files/routes-adjara-2.kml", {
-        map: map,
-        suppressInfoWindows: true,
-        preserveViewport: false,
-      });
+    // var kmlLayer = new google.maps.KmlLayer("https://nikajamburia.github.io/files/routes-adjara-2.kml", {
+    //     map: map,
+    //     suppressInfoWindows: true,
+    //     preserveViewport: false,
+    //   });
 
-    kmlLayer.addListener('click', function(event) {
-        console.log(event);
-        var content = event.featureData.infoWindowHtml;
-        var testimonial = document.getElementById('capture');
-        testimonial.innerHTML = content;
-        console.log(getRouteCode(event.featureData.description));
-    });
+    // kmlLayer.addListener('click', function(event) {
+    //     console.log(event);
+    //     console.log(event.latLng.lat());
 
-    // var allRoutesData = new google.maps.Data({map: map});
-    // allRoutesData.loadGeoJson(
-    //     "https://nikajamburia.github.io/files/routes-adjara-sm.geojson"
-    // );
-
-    // allRoutesData.setStyle({
-    //     strokeColor: 'green',
-    //     strokeWeight: 3
-    // });
-    
-    // allRoutesData.addListener('click', function(event) {
-    //     allRoutesData.overrideStyle(event.feature, {strokeColor: 'red'});
-
-    //     console.log(event.feature.g.g[1]);
-
-
-    //     var routeMiddle = event.feature.g.g[Math.round((event.feature.g.g.length - 1) / 2)];
-    //     console.log(routeMiddle);
-    //     map.setCenter({ lat: routeMiddle.lat, lng: routeMiddle.lng });
+    //     map.setCenter( event.latLng );
     //     map.setZoom(12);
 
-    //     console.log(event.feature);
+    //     var content = event.featureData.infoWindowHtml;
+    //     var testimonial = document.getElementById('capture');
+    //     testimonial.innerHTML = content;
+    //     console.log(getRouteCode(event.featureData.description));
     // });
+
+    var allRoutesData = new google.maps.Data({map: map});
+    allRoutesData.loadGeoJson(
+        "https://nikajamburia.github.io/files/routes-adjara-2.geojson"
+    );
+
+    allRoutesData.setStyle({
+        strokeColor: '#ff0051e6',
+        strokeWeight: 3
+    });
+    
+    allRoutesData.addListener('click', function(event) {
+        allRoutesData.overrideStyle(event.feature, {strokeColor: 'green'});
+
+  
+        map.setCenter( event.latLng );
+        map.setZoom(12);
+
+        var content = event.feature.i.description;
+        console.log(event.feature.i.routeCode);
+        document.getElementById('capture').innerHTML = content;
+
+    });
 
   }
 
   function getRouteCode(content) {
-      var replaced = content.replace('მარშრუტის კოდი:', '');
-      console.log(replaced);
-      return content
-        .replace('მარშრუტის კოდი:', '')
-        .substring(0, content.indexOf("<br>"))
+
+      var trimBeggining = content.replace('მარშრუტის კოდი:', '')
+      return trimBeggining
+        .substring(0, trimBeggining.indexOf("<br>"))
         .trim();
   }
